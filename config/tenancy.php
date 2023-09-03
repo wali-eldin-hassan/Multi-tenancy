dd64170e1f3e2b5a9e60d66a503f94ee8eefaf89
wali-eldin-hassan <creaspo6@gmail.com>
2023-09-04 01:16:43 +0300

    config tenant model

--- config/tenancy.php
+++ config/tenancy.php
@@ -2,8 +2,8 @@
 
 declare(strict_types=1);
 
+use App\Models\Tenant;
 use Stancl\Tenancy\Database\Models\Domain;
-use Stancl\Tenancy\Database\Models\Tenant;
 
 return [
     'tenant_model' => Tenant::class,
